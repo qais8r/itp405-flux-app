14fc4e2634f18eb8af631ae51cc03af8d1538f0f
Qaisar Sulaiman <qsulaima@usc.edu>
2025-05-02 22:01:38 -0700

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
