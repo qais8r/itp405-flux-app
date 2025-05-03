70695cec96ec7bc304186a9024b27efcd6aa6dc4
Qaisar Sulaiman <qsulaima@usc.edu>
2025-05-02 22:04:37 -0700

    Add authentication to web routes

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,38 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\AuthController;
 
-// Home → list of posts
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+// Authentication
+Route::get('register',      [AuthController::class, 'showRegister'])->name('register');
+Route::post('register',     [AuthController::class, 'register']);
+Route::get('login',         [AuthController::class, 'showLogin'])->name('login');
+Route::post('login',        [AuthController::class, 'login']);
+Route::post('logout',       [AuthController::class, 'logout'])->name('logout');
 
-// Posts
-Route::get('/posts',                 [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create',          [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts',                [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}',          [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts/{post}/edit',     [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}',          [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}',       [PostController::class, 'destroy'])->name('posts.destroy');
+Route::middleware('auth')->group(function () {
 
-// Comments (nested under posts for creation)
-Route::post('/posts/{post}/comments',          [CommentController::class, 'store'])->name('comments.store');
-Route::get('/comments/{comment}/edit',         [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/comments/{comment}',              [CommentController::class, 'update'])->name('comments.update');
-Route::delete('/comments/{comment}',           [CommentController::class, 'destroy'])->name('comments.destroy');
+    // Home → list of posts
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-// Favorites / Bookmarks
-Route::get('/favorites',                       [FavoriteController::class, 'index'])->name('favorites.index');
-Route::post('/posts/{post}/favorite',          [FavoriteController::class, 'store'])->name('favorites.store');
-Route::delete('/posts/{post}/favorite',        [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+    // Posts
+    Route::get('/posts',                 [PostController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create',          [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts',                [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}',          [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts/{post}/edit',     [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}',          [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}',       [PostController::class, 'destroy'])->name('posts.destroy');
+
+    // Comments (nested under posts for creation)
+    Route::post('/posts/{post}/comments',          [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{comment}/edit',         [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{comment}',              [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}',           [CommentController::class, 'destroy'])->name('comments.destroy');
+
+    // Favorites / Bookmarks
+    Route::get('/favorites',                       [FavoriteController::class, 'index'])->name('favorites.index');
+    Route::post('/posts/{post}/favorite',          [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('/posts/{post}/favorite',        [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+
+});
