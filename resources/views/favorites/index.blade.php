98e2963e53a59d14fa0fcd3b9f0c17c3d2150875
Qaisar Sulaiman <qsulaima@usc.edu>
2025-05-03 16:23:41 -0700

    Updated favorites grid layout to match that of post's

--- resources/views/favorites/index.blade.php
+++ resources/views/favorites/index.blade.php
@@ -14,7 +14,7 @@
                     feed</a> and find some you like?</p>
         </div>
     @else
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4">
             @foreach ($favorites as $post)
                 <div class="col">
                     <div class="card h-100 shadow-sm border-light">
